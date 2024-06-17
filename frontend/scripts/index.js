43ba0794398d78b980b6fc49410240332160cd14
Clarence Jordan <clarencejordan087@gmail.com>
2024-06-17 14:37:41 -0500

    add form

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,1 +1,12 @@
+import { getCategories } from "./services/create_routes.js";
 
+async function handleCategories() {
+  try {
+    let res = await getCategories();
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+handleCategories();
--- frontend/scripts/new_todos.js
+++ frontend/scripts/new_todos.js
@@ -1,1 +1,15 @@
-console.log("working");
+import { newTodoCatagoryDropDown } from "./helpers/dorpdown.js";
+import { getCategories } from "./services/create_routes.js";
+const catagoryDropDown = document.querySelector("#category");
+async function handleCategories() {
+  try {
+    let res = await getCategories();
+    res.unshift({ id: 0, name: "none" });
+
+    newTodoCatagoryDropDown(catagoryDropDown, res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+handleCategories();
