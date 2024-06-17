b38bf4b24fe32b6ede07ea7b665567bacfbaac94
Adriana Kendricks <kendricksadriana16@gmail.com>
2024-06-17 13:19:45 -0500

    updated nav

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,1 +1,5 @@
 
+let node = document.createElement("li");
+let textnode = document.createTextNode("Scavenger Hunt");
+node.appendChild(textnode);
+document.getElementById("activities").appendChild(node);
