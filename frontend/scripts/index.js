ce46bd46e73501d1910046824df3f86d21cbf325
Adriana Kendricks <kendricksadriana16@gmail.com>
2024-06-18 10:59:51 -0500

    fixed

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,5 +1,2 @@
+"use strict"
 
-let node = document.createElement("li");
-let textnode = document.createTextNode("Scavenger Hunt");
-node.appendChild(textnode);
-document.getElementById("activities").appendChild(node);
